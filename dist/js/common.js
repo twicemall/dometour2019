f7b1c5b899c29935bf13b74026e160b9116fee8a
Polly <twicemall.tw@gmail.com>
2019-02-19 21:58:04 +0800

    Update common.js

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,5 +1,6 @@
 // $(document).ready(function() {});
 // https://css-tricks.com/snippets/jquery/run-javascript-only-after-entire-page-has-loaded/
+// https://stackoverflow.com/questions/16540136/javascript-how-to-remove-the-last-character-from-a-div-or-a-string
 $(window).bind("load", function() {
   $('.form-heading').text(function (_,txt) {
     return txt.slice(3, -1);

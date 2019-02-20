a18c3d54207a8f6849ef5a17e6369e4df037ce96
Polly <twicemall.tw@gmail.com>
2019-02-20 09:16:11 +0800

    Refine string

--- dist/js/common.js
+++ dist/js/common.js
@@ -5,4 +5,6 @@ $(window).bind("load", function() {
   $('.form-heading').text(function (_,txt) {
     return txt.slice(3, -1);
   });
+  $(".label-at-top[for=productName]:contains('商品樣式')").html('請選擇樣式');
+  $(".label-at-top[for=productQuantity]:contains('購買數量')").html('請選擇數量');
 });
